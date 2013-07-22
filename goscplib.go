b98a0ef14b24adcad475ca3548e43207adcaeb27
gnicod <ovskywalker@gmail.com>
2013-07-22 22:43:08 +0200

    add recursive mode

--- goscplib.go
+++ goscplib.go
@@ -6,13 +6,15 @@ import(
 	"log"
 	"os"
 	"io"
+	"io/ioutil"
 	"path/filepath"
 )
 
 //Constants
 const (
 	SCP_PUSH_BEGIN_FILE   = "C0644"
-	SCP_PUSH_BEGIN_FOLDER = "D0755"
+	SCP_PUSH_BEGIN_FOLDER = "D0755 0"
+	SCP_PUSH_END_FOLDER   = "E"
 	SCP_PUSH_END          = "\x00"
 )
 
@@ -59,6 +61,53 @@ func (scp *Scp) PushFile (src string, dest string) error{
 }
 
 //Push directory to server
-func (scp *Scp) PushDir (src string, dest string) {
-	fmt.Println("not implemented yet")
+func (scp *Scp) PushDir (src string, dest string) error{
+	session, err := scp.client.NewSession()
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+	go func() {
+		w, _ := session.StdinPipe()
+		//w := os.Stdout
+		defer w.Close()
+		fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER +" "+ filepath.Base(dest))
+		lsDir(w,src)
+		fmt.Fprintln(w,SCP_PUSH_END_FOLDER)
+
+	}()
+	if err := session.Run("/usr/bin/scp -qrt "+dest); err != nil {
+		return err
+	}
+	return nil
+}
+
+func prepareFile(w io.WriteCloser,src string){
+		fileSrc, srcErr := os.Open(src)
+		if srcErr != nil {
+			log.Fatalln("Failed to open source file: " + srcErr.Error())
+		}
+		//Get file size
+		srcStat, statErr := fileSrc.Stat()
+		if statErr != nil {
+			log.Fatalln("Failed to stat file: " + statErr.Error())
+		}
+		// Print the file content
+		fmt.Fprintln(w, SCP_PUSH_BEGIN_FILE, srcStat.Size(), filepath.Base(src))
+		io.Copy(w, fileSrc)
+		fmt.Fprint(w, SCP_PUSH_END)
+}
+
+func lsDir(w io.WriteCloser,dir string){
+	fi,_ := ioutil.ReadDir(dir)
+	//parcours des dossiers
+	for _, f := range fi {
+		if f.IsDir(){
+			fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER +" "+ f.Name())
+			lsDir(w,dir+"/"+f.Name())
+			fmt.Fprintln(w,SCP_PUSH_END_FOLDER)
+		}else{
+			prepareFile(w,dir+"/"+f.Name())
+		}
+	}
 }
