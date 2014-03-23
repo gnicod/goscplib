c5aa6597dfee05a313bdb108dfab9d8bc53d2673
gnicod <nicod.ga@gmail.com>
2014-03-23 01:49:01 +0100

    take care of filemode

--- goscplib.go
+++ goscplib.go
@@ -12,14 +12,21 @@ import(
 
 //Constants
 const (
-	SCP_PUSH_BEGIN_FILE   = "C0644"
-	SCP_PUSH_BEGIN_FOLDER = "D0755 0"
-	SCP_PUSH_END_FOLDER   = "E"
-	SCP_PUSH_END          = "\x00"
+  SCP_PUSH_BEGIN_FILE       = "C"
+  SCP_PUSH_BEGIN_FOLDER     = "D"
+  SCP_PUSH_BEGIN_END_FOLDER = " 0"
+  SCP_PUSH_END_FOLDER       = "E"
+  SCP_PUSH_END              = "\x00"
 )
 
 type Scp struct {
 	client *ssh.ClientConn
+}
+
+func GetPerm(f *os.File) (perm string){
+  fileStat, _ := f.Stat()
+  mod := fileStat.Mode()
+  return fmt.Sprintf("%#o" , uint32(mod))
 }
 
 //Initializer
@@ -40,6 +47,7 @@ func (scp *Scp) PushFile (src string, dest string) error{
 		w, _ := session.StdinPipe()
 		defer w.Close()
 		fileSrc, srcErr := os.Open(src)
+    //fileStat, err := fileSrc.Stat()
 		if srcErr != nil {
 			log.Fatalln("Failed to open source file: " + srcErr.Error())
 		}
@@ -50,7 +58,7 @@ func (scp *Scp) PushFile (src string, dest string) error{
 		}
 		// According to https://blogs.oracle.com/janp/entry/how_the_scp_protocol_works
 		// Print the file content
-		fmt.Fprintln(w, SCP_PUSH_BEGIN_FILE, srcStat.Size(), filepath.Base(dest))
+		fmt.Fprintln(w, SCP_PUSH_BEGIN_FILE, GetPerm(fileSrc), srcStat.Size(), filepath.Base(dest))
 		io.Copy(w, fileSrc)
 		fmt.Fprint(w, SCP_PUSH_END)
 	}()
@@ -71,7 +79,8 @@ func (scp *Scp) PushDir (src string, dest string) error{
 		w, _ := session.StdinPipe()
 		//w := os.Stdout
 		defer w.Close()
-		fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER +" "+ filepath.Base(dest))
+    folderSrc, _ := os.Open(src)
+		fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER ,GetPerm(folderSrc),SCP_PUSH_BEGIN_END_FOLDER," ", filepath.Base(dest))
 		lsDir(w,src)
 		fmt.Fprintln(w,SCP_PUSH_END_FOLDER)
 
@@ -93,7 +102,7 @@ func prepareFile(w io.WriteCloser,src string){
 			log.Fatalln("Failed to stat file: " + statErr.Error())
 		}
 		// Print the file content
-		fmt.Fprintln(w, SCP_PUSH_BEGIN_FILE, srcStat.Size(), filepath.Base(src))
+		fmt.Fprintln(w, SCP_PUSH_BEGIN_FILE, GetPerm(fileSrc), srcStat.Size(), filepath.Base(src))
 		io.Copy(w, fileSrc)
 		fmt.Fprint(w, SCP_PUSH_END)
 }
@@ -103,7 +112,8 @@ func lsDir(w io.WriteCloser,dir string){
 	//parcours des dossiers
 	for _, f := range fi {
 		if f.IsDir(){
-			fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER +" "+ f.Name())
+      folderSrc, _ := os.Open(dir+"/"+f.Name())
+			fmt.Fprintln(w,SCP_PUSH_BEGIN_FOLDER , GetPerm(folderSrc),SCP_PUSH_BEGIN_END_FOLDER," ", f.Name())
 			lsDir(w,dir+"/"+f.Name())
 			fmt.Fprintln(w,SCP_PUSH_END_FOLDER)
 		}else{
